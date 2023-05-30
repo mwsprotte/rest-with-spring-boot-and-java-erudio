7c0f08aa17c9760f97c70426c7e6a0fca22bdfa2
msprotte <msprotte@weg.net>
2023-05-30 18:24:27 -0300

    update upload

---
+++ src/main/java/br/com/erudio/controllers/FileController.java
@@ -0,0 +1,46 @@
+package br.com.erudio.controllers;
+
+import br.com.erudio.data.vo.v1.UploadFileResponseVO;
+import br.com.erudio.services.FileStorageService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
+
+@Tag(name = "File Import")
+@RestController
+@RequestMapping("/api/file/v1")
+public class FileController {
+
+    private Logger logger = Logger.getLogger((FileController.class.getName()));
+
+    @Autowired
+    private FileStorageService service;
+
+    @PostMapping("/uploadFile")
+    public UploadFileResponseVO uploadFile(@RequestParam("file") MultipartFile file) {
+        logger.info("Storing file to disk");
+
+        var filename = service.storeFile(file);
+        String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/file/v1/downloadFile/").path(filename).toUriString();
+
+        return new UploadFileResponseVO(filename, fileDownloadUri, file.getContentType(), file.getSize());
+    }
+
+    @PostMapping("/uploadMultipleFiles")
+    public List<UploadFileResponseVO> uploadMultipleFiles(@RequestParam("files") MultipartFile[] files) {
+        logger.info("Storing file to disk");
+
+        return Arrays.asList(files).stream().map(file -> uploadFile(file)).collect(Collectors.toList());
+    }
+
+}
